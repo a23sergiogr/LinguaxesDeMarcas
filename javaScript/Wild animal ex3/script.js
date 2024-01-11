572a0b6198db39f4e4fe4e115cc321a0aab8f7fe
a23sergiogr <a23sergiogr@iessanclemente.net>
2024-01-11 11:55:09 +0100

    ex3

---
+++ javaScript/Wild animal ex3/script.js
@@ -0,0 +1,36 @@
+function check(){
+    let name=document.getElementById("name");
+    let result=document.getElementById("result");
+
+    if (name.value == "butterfly"){
+        result.style = "visibility: visible"
+        result.style.borderColor = "#498B5A"
+        result.style.backgroundColor = "#615443"
+        result.style.color = "#2BE058" 
+
+        result.textContent = "Bien hecho"
+
+
+    }
+    else{
+        result.style = "visibility: visible"
+        result.style.borderColor = "#8B403A"
+        result.style.backgroundColor = "#384B61"
+        result.style.color = "#E02212" 
+
+        result.textContent = "Intenta otra vez"
+    }
+
+    
+}
+
+function reset(){
+    let name=document.getElementById("name");
+    let result=document.getElementById("result");
+
+    name.value = ""
+
+    result.style = "visibility: hidden"
+}
+
+//butterfly
--- javaScript/counter ex2/script.js
+++ javaScript/counter ex2/script.js
@@ -1,12 +1,16 @@
 function increse(){
     let number=document.getElementById("number");
     number.innerText = parseInt(number.innerText) + 1;
+    number.style.color = "green"
 }
 function decrese(){
     let number=document.getElementById("number");
     number.innerText = parseInt(number.innerText) - 1;
+    number.style.color = "red"
 }
 function reset(){
     let number=document.getElementById("number");
     number.innerText = 0;
+    number.style.color = "white"
 }
+
